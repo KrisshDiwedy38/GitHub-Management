55e06031e4bb318a506842cd6cf2b49a8b45dbdf
Krissh Diwedy <krisshdiwedy38@gmail.com>
2025-02-13 02:17:49 +0530

    First Commit

---
+++ Github_Management.py
@@ -0,0 +1,71 @@
+import requests 
+import os 
+import sys
+from github_token import gitHubToken
+
+def cont():
+   while True:
+      ask = input("Want to exit (y/n)? ")
+      if ask == 'y':
+         print("----- Exiting -----")
+         return False
+      elif ask == 'n':
+         return True
+      else:
+         print("Wrong input")
+         cont()
+
+def get_token():
+   #Getting the User's Github Token
+   token = input("Enter your personal Github Token: ").strip()
+   return token
+
+def get_headers(token):
+   # Getting headers for Authorization process 
+   return {
+         "Authorization": f"Bearer {token}",
+         "Accept": "application/vnd.github+json"
+      }
+
+def create_repo(token):
+   pass
+
+def create_issue(token):
+   pass
+
+def list_repo(token):
+   pass
+
+def delete_repo(token):
+   pass
+
+def menu_system(token):
+   print("\nChoose an option:")
+   print(" Create a Repository Press 1")
+   print(" List Your Repositories Press 2")
+   print(" Create an Issue Press 3")
+   print(" Delete a Repository Press 4")
+    
+   task = input("Enter Your Choice").lower().strip()
+
+   if task == 1:
+      create_repo(token)
+   elif task == 2:
+      list_repo(token)
+   elif task == 3:
+      create_issue(token)
+   elif task == 4:
+      delete_repo(token)
+   else:
+      print("Wrong Input")
+      menu_system()
+
+
+def main():
+   token = get_token()
+   menu_system(token)
+   cont()
+
+
+if __name__ == "__main__":
+   main()
